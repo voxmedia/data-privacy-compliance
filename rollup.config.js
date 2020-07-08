553dde29e0ca94f7f507ca459a5ea159f9536b42
Brian Anderson <brian.anderson@voxmedia.com>
2020-07-07 22:40:31 -0700

    removed outdated copy and paste comment

--- rollup.config.js
+++ rollup.config.js
@@ -4,15 +4,15 @@ import babel from '@rollup/plugin-babel';
 import pkg from './package.json';
 
 export default [
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
     input: 'src/index.js',
-    output: [{ file: pkg.main, format: 'cjs', sourcemap: true }],
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
     plugins: [
       resolve(),
       commonjs(),
